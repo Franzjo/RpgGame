75b00527b1f24afdb7533b6a34a38d4f935a108e
akino <akino.07007@gmail.com>
2021-04-02 12:31:15 -0600

    Proyecto inicial

---
+++ RPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    Ray lastRay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
